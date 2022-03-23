c940c13b1b2b23849500e1f900164ad027c425d2
ichirokira <ichirokira@gmail.com>
2022-03-23 12:05:57 +0900

    Add pyramid transform

--- modelling/transformation/__init__.py
+++ modelling/transformation/__init__.py
@@ -2,3 +2,4 @@ from modelling.transformation.entangle import entangler
 from modelling.transformation.rotation import layerY, layerX, layerZ
 from modelling.transformation.hardware_efficient import HE, HE_Color_Independence
 from modelling.transformation.farhi import Farhi
+from modelling.transformation.block_transform import Pyramid_Transform
